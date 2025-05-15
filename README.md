a01204aec1adfb82ff9c5c21e1124870e13c89b0
Miyuanhao <15554810985@163.com>
2025-05-15 23:35:47 +0800

    feat: 添加 GTA-lora 模型
    - 使用 Fluxgym 训练的 GTA6 风格 Lora 模型
    - 包含训练配置、触发词和样本图像
    - 添加 README.md 和 dataset.toml 文件
    - 包含多个样本图像和模型权重文件

---
+++ README.md
@@ -0,0 +1,31 @@
+---
+tags:
+- text-to-image
+- flux
+- lora
+- diffusers
+- template:sd-lora
+- fluxgym
+widget:
+-   output:
+        url: sample/gta-loragta-lora_003500_00_20250515121941.png
+    text: A fierce man with dreadlocks wearing sunglasses is walking along the beach.
+base_model: black-forest-labs/FLUX.1-dev
+
+license: other
+license_name: flux-1-dev-non-commercial-license
+license_link: https://huggingface.co/black-forest-labs/FLUX.1-dev/blob/main/LICENSE.md
+---
+
+# GTA-lora
+
+使用[Fluxgym](https://github.com/cocktailpeanut/fluxgym)训练的GTA6-style-lora。
+## Train config/训练设置
+- 70张官方高质量4k图片
+- 每张图片重复次数:6
+- 训练轮次:3500
+
+## Trigger words/触发词
+
+No trigger words defined.
+没有设定触发词
---
+++ train.bat
@@ -0,0 +1,39 @@
+accelerate launch ^
+  --mixed_precision bf16 ^
+  --num_cpu_threads_per_process 1 ^
+  sd-scripts/flux_train_network.py ^
+  --pretrained_model_name_or_path "D:\fluxgym\models\unet\flux1-dev.sft" ^
+  --clip_l "D:\fluxgym\models\clip\clip_l.safetensors" ^
+  --t5xxl "D:\fluxgym\models\clip\t5xxl_fp16.safetensors" ^
+  --ae "D:\fluxgym\models\vae\ae.sft" ^
+  --cache_latents_to_disk ^
+  --save_model_as safetensors ^
+  --sdpa --persistent_data_loader_workers ^
+  --max_data_loader_n_workers 2 ^
+  --seed 42 ^
+  --gradient_checkpointing ^
+  --mixed_precision bf16 ^
+  --save_precision bf16 ^
+  --network_module networks.lora_flux ^
+  --network_dim 4 ^
+  --optimizer_type adafactor ^
+  --optimizer_args "relative_step=False" "scale_parameter=False" "warmup_init=False" ^
+  --lr_scheduler constant_with_warmup ^
+  --max_grad_norm 0.0 ^--sample_prompts="D:\fluxgym\outputs\gta-loragta-lora\sample_prompts.txt" --sample_every_n_steps="2" ^
+  --learning_rate 8e-4 ^
+  --cache_text_encoder_outputs ^
+  --cache_text_encoder_outputs_to_disk ^
+  --fp8_base ^
+  --highvram ^
+  --max_train_epochs 5 ^
+  --save_every_n_epochs 4 ^
+  --dataset_config "D:\fluxgym\outputs\gta-loragta-lora\dataset.toml" ^
+  --output_dir "D:\fluxgym\outputs\gta-loragta-lora" ^
+  --output_name gta-loragta-lora ^
+  --timestep_sampling shift ^
+  --discrete_flow_shift 3.1582 ^
+  --model_prediction_type raw ^
+  --guidance_scale 2.5 ^
+  --loss_type l2 ^
+  --train_batch_size 4 ^
+  --xformers
